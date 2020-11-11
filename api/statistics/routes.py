2a5534a71141e105f4f1fa62a812ee6fb148f027
yurii <jurakulek@gmail.com>
2020-11-11 22:33:11 +0200

    Updated service

--- api/statistics/routes.py
+++ api/statistics/routes.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from motor.motor_asyncio import AsyncIOMotorClient
-from fastapi import Depends
+from fastapi import Depends, status
+from fastapi.responses import Response
 from fastapi.routing import APIRouter
 
 from api.statistics.schemas import ResponseOptionSchema, CreateUpdateOptionSchema
@@ -11,7 +12,9 @@ from core.database import get_database
 router = APIRouter()
 
 
-@router.post('', response_model=ResponseOptionSchema)
-async def create_or_update_option_route(options: List[CreateUpdateOptionSchema],
-                                        conn: AsyncIOMotorClient = Depends(get_database)):
-    return await create_or_update_option(conn, options)
+@router.post('', response_model=ResponseOptionSchema, status_code=status.HTTP_204_NO_CONTENT)
+async def create_or_update_options_route(options: List[CreateUpdateOptionSchema],
+                                         conn: AsyncIOMotorClient = Depends(get_database)):
+    for option in options:
+        await create_or_update_option(conn, option)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- api/statistics/services.py
+++ api/statistics/services.py
@@ -11,10 +11,10 @@ async def create_or_update_option(conn: AsyncIOMotorClient,
     data = {'option_id': option_data.option_id, 'poll_id': option_data.poll_id}
     option = await conn[MONGO_INITDB_DATABASE][COLLECTION_NAME].find_one(data)
     increment_data = {
-        EventType.SELECTED.name: {'selected_times': 1},
-        EventType.WON.name: {'won_times': 1},
-        EventType.TOOK_PART.name: {'took_part_times': 1},
-        EventType.TOOK_PART_IN_POLL.name: {'took_part_in_poll_times': 1},
+        EventType.SELECTED.value: {'selected_times': 1},
+        EventType.WON.value: {'won_times': 1},
+        EventType.TOOK_PART.value: {'took_part_times': 1},
+        EventType.TOOK_PART_IN_POLL.value: {'took_part_in_poll_times': 1},
     }
     if option:
         option = await conn[MONGO_INITDB_DATABASE][COLLECTION_NAME].update_one(
@@ -32,11 +32,9 @@ async def get_calculated_options(poll_id: int, conn: AsyncIOMotorClient,
     options = await conn[MONGO_INITDB_DATABASE][COLLECTION_NAME].find(
         {'poll_id': poll_id}).skip((page - 1) * page_size).limit(page_size)
     result = [CalculatedOptionSchema(option_id=option.option_id,
-                                     win_percentage=get_percentage(option.took_part_times,
+                                     win_percentage=get_percentage(option.took_part_in_poll_times,
                                                                    option.win_times),
                                      selected_percentage=get_percentage(option.took_part_times,
                                                                         option.selected_times))
               for option in options]
     return result
-
-
