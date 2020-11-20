23772fafae3ceaa51604ea78ab66c28984531497
yurii <jurakulek@gmail.com>
2020-11-20 22:28:26 +0200

    Updated entrypoint;

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #!/bin/sh
 
-poetry run gunicorn main:app -k uvicorn.workers.UvicornWorker -b 0.0.0.0:80 --workers=4 --log-level=info
+poetry run gunicorn main:app -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 --workers=4 --log-level=info
 
 exec "$@"
